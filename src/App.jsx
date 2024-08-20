a398587312eb45abf77c9419dc67bd77933f6d41
sebastien lau <sebastien.lau2021@gmail.com>
2024-08-20 14:51:49 +0200

    add context + simulate fetch data from data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,60 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import { Suspense } from "react"
+import { Suspense, useState, useEffect } from "react"
 import { Home, Housing, Error, About } from "./pages"
 import { Layout } from "./components"
+import { ProductContext } from "./contexts"
 
 function App() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch("/public/data.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+        setError(error)
+        setLoading(false)
+      })
+  }, [])
+
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Layout>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/housing/:id" element={<Housing />} />
-            <Route path="*" element={<Error />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-          {/* <div>
+    <ProductContext.Provider value={{ data, setData }}>
+      {error ? (
+        <Error />
+      ) : loading ? (
+        <p>Loading</p>
+      ) : (
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Layout>
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/housing/:id" element={<Housing />} />
+                <Route path="*" element={<Error />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+              {/* <div>
             <Link to="/home">Home</Link>
             <Link to="/housing/1">Housing</Link>
             <Link to="/about">About</Link>
             <Link to="/zsdfvgbn">Error</Link>
           </div> */}
-        </Layout>
-      </Suspense>
-    </Router>
+            </Layout>
+          </Suspense>
+        </Router>
+      )}
+    </ProductContext.Provider>
   )
 }
 
---
+++ src/contexts/index.js
@@ -0,0 +1,3 @@
+import ProductContext from "./product_context"
+
+export { ProductContext }
