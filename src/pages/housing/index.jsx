1d78929fba51a5e556428e9a22eff703740346fe
sebastien lau <sebastien.lau2021@gmail.com>
2024-08-25 11:38:54 +0200

    add rating to property page

--- src/pages/housing/index.jsx
+++ src/pages/housing/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { ProductContext } from "../../contexts"
 import { useParams } from "react-router-dom"
 
+import Rating from "./rating"
 import Carrousel from "./carrousel"
 
 const Housing = () => {
@@ -11,6 +12,24 @@ const Housing = () => {
   return (
     <div>
       <Carrousel images={property.pictures} />
+      <div>
+        <h1>{property.title}</h1>
+        <p>{property.location}</p>
+        <div className="host">
+          <p>{property.host.name}</p>
+          <img src={property.host.picture} alt={property.host.name} />
+        </div>
+      </div>
+      <div className="tag-rate">
+        <div className="tags">
+          {property.tags.map((tag, i) => (
+            <span key={i}>{tag}</span>
+          ))}
+        </div>
+        <div className="rate">
+          <Rating rate={property.rating} />
+        </div>
+      </div>
     </div>
   )
 }
