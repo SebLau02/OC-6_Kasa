6a5d64fc7f0a3fd1f9b10b384d4d0ca92c9444c6
Sébastien Lau <sebastien.lau2021@gmail.com>
2024-09-02 17:32:20 +0200

    add fixes

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
     <div id="error">
       <span>404</span>
-      <h1>Oups! La page que vous demandez nexiste pas</h1>
+      <h1>Oups! La page que vous demandez n'existe pas.</h1>
       <Link to="/">Retour sur la page d'accueil</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
